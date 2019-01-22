a76fb3ff3e702cf956efbf71587c327fe3e48b05
MaksimEgorov <egorov.gps@gmail.com>
2019-01-22 19:14:01 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,52 +1,43 @@
- function plusAdults() {
-     var current = document.getElementById('adults').value;
-     current++;
-     document.getElementById('adults').value = current;
- }
+var adults = document.getElementById('adults');
 
- function minusAdults() {
-     var current = document.getElementById('adults').value;
-     if (current > 0) {
-         current--
-     } else {
-         return
-     };
+function plusAdults() {
+    var current = adults.value;
+    current++;
+    adults.value = current;
+    //   adults.value++; - альтернативный вариант
+}
 
-     document.getElementById('adults').value = current;
- }
+function minusAdults() {
+    var current = adults.value;
+    if (current > 0) {
+        current--
+    };
+    adults.value = current;
+}
 
+function plus() {
+    var current = document.getElementById('kids').value;
+    current++;
+    document.getElementById('kids').value = current;
+}
 
- function plus() {
-     var current = document.getElementById('kids').value;
-     current++;
-     document.getElementById('kids').value = current;
- }
+function minus() {
+    var current = document.getElementById('kids').value;
+    if (current > 0) {
+        current--
+    } else {
+        return
+    };
 
- function minus() {
-     var current = document.getElementById('kids').value;
-     if (current > 0) {
-         current--
-     } else {
-         return
-     };
+    document.getElementById('kids').value = current;
+}
 
-     document.getElementById('kids').value = current;
- }
+function openModalSearch() {
+    document.querySelector('.modal').classList.remove('hidden');
+}
 
- function openModalSearch() {
-     var openSearch = document.querySelector('.intrested__order--src_btn');
-     var modalSearch = document.querySelector('.modal');
-
-     openSearch.addEventListener('click', function () {
-         modalSearch.classList.remove('hidden');
-     })
- }
-
- function closeModalSearch() {
-     var closeSearch = document.querySelector('.modal-close');
-     var modalSearch = document.querySelector('.modal');
-
-     closeSearch.addEventListener('click', function () {
-         modalSearch.classList.add('hidden');
-     })
- }
+var closeSearch = document.querySelector('.modal-close');
+var modalSearch = document.querySelector('.modal');
+closeSearch.addEventListener('click', function () {
+    modalSearch.classList.add('hidden');
+})
